6c96d2482db993a783a980e213e14cdf564881fc
evshary <evshary@gmail.com>
2019-10-16 13:36:55 +0800

    Able to detect whether add "source ~/.ros_bashrc".

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+shell=`echo $SHELL | awk -F '/' '{print $NF}'`
+
 rm ~/.ros_bashrc
 rm ~/.extra_bashrc
 ln -s `pwd`/ros_bashrc ~/.ros_bashrc
 ln -s `pwd`/extra_bashrc ~/.extra_bashrc
-echo "Remember to add 'source ~/.ros_bashrc' to your .bashrc"
+
+if ! grep -q ros_bashrc ~/.${shell}rc; then
+    echo "source ~/.ros_bashrc" >> ~/.${shell}rc
+fi
+
+echo "Installation done!"
