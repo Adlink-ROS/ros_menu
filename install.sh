fb78ffdf2f717b9901d72fe0ea211cee10d94ae2
ChenYing Kuo <evshary@gmail.com>
2020-08-09 17:26:40 +0800

    Able to install ROS while installing ROS menu.

--- install.sh
+++ install.sh
@@ -2,6 +2,31 @@
 
 shell=`echo $SHELL | awk -F '/' '{print $NF}'`
 
+echo -n "Do you want to install ROS automatically?(y/N): "
+read ros_install
+if [ "$ros_install" '==' "y" ];
+then
+    # Install ROS 1 melodic
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+    sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+    sudo apt update
+    sudo apt install -y ros-melodic-desktop-full
+    # Install ROS 1 build tools
+    sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+    # Install ROS 2 dashing
+    sudo locale-gen en_US en_US.UTF-8
+    sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+    export LANG=en_US.UTF-8
+    sudo apt update && sudo apt install -y curl gnupg2 lsb-release
+    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+    sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
+    sudo apt update
+    sudo apt install -y ros-dashing-desktop
+    sudo apt install -y python3-argcomplete
+    # Install ROS 2 build tools
+    sudo apt install python3-colcon-common-extensions python3-vcstool
+fi
+
 rm -f ~/.ros_menu
 ln -s `pwd` ~/.ros_menu
 
