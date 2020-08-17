3948b09f17a23bd03664804f752805b733a903f8
Ting Chang <ting.chang@adlinktech.com>
2020-08-17 12:43:25 +0800

    Separate rosdistros into different scripts (#5)

--- install.sh
+++ install.sh
@@ -9,66 +9,13 @@ read ros_install
 if [ "$ros_install" '==' "y" ] || [ "$ros_install" '==' "Y" ];
 then
     # Install ROS 1 melodic
-    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-    sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-    sudo apt update
-    sudo apt install -y ros-$ros1_distro-desktop-full
-
-    # Install ROS 1 build tools
-    sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+    ./scripts/install_melodic.sh
 
     # Install ROS 2 dashing
-    sudo locale-gen en_US en_US.UTF-8
-    sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-    export LANG=en_US.UTF-8
-    sudo apt update && sudo apt install -y curl gnupg2 lsb-release
-    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-    sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
-    sudo apt update
-    sudo apt install -y ros-$ros2_distro-desktop
-    sudo apt install -y python3-argcomplete
-    
-    # Install ROS 2 RQT
-    sudo apt install -y ros-$ros2_distro-rqt
-    
-    # Install turtlesim for verification
-    sudo apt install -y ros-$ros2_distro-turtlesim
-    
-    # Install ROS 2 build tools
-    sudo apt install -y python3-colcon-common-extensions python3-vcstool
+    ./scripts/install_dashing.sh
 
-    # RMW for ROS 2
-    sudo apt install -y ros-$ros2_distro-rmw-cyclonedds-cpp ros-$ros2_distro-rmw-opensplice-cpp
-
-    # ros1_bridge
-    sudo apt install -y ros-$ros2_distro-ros1-bridge
-
-    # Install ROS dependencies (for Dashing and Eloquent on Ubuntu 18.04)
-    sudo apt install -y python-rosdep 
-
-    # Install ROS dependencies (for Foxy on Ubuntu 20.04)
-    # sudo apt install -y python3-rosdep
-
-    # Install Common Linux Tools
-    sudo apt install -y \
-	    build-essential \
-	    cmake \
-	    git \
-	    libbullet-dev \
-	    python3-argcomplete \
-	    python3-colcon-common-extensions \
-	    python3-flake8 \
-	    python3-pip \
-	    python3-pytest-cov \
-	    python3-setuptools \
-	    python3-vcstool \
-	    openssh-server \
-	    byobu \
-	    wget
-
-    # Initialize and update rosdep
-    sudo rosdep init
-    rosdep update
+    # Install ROS dependencies and related packages
+    ./scripts/install_dep.sh
 
 else
     echo "Skip installing ROS"
