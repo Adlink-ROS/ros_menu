f5e792456741558dffedfa1b141d98b0748be448
ChenYing Kuo <evshary@gmail.com>
2021-07-08 10:36:59 +0800

    Install necessary packages before installing melodic

--- scripts/install_melodic.sh
+++ scripts/install_melodic.sh
@@ -3,6 +3,7 @@
 ros1_distro=melodic
 
     # Install ROS 1
+    sudo apt update && sudo apt install -y curl gnupg2 lsb-release
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
     curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
     sudo apt update
