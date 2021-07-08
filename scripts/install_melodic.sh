8ca53839001bd5b6cb754de286f7e12c6a7bc37a
ChenYing Kuo <evshary@gmail.com>
2021-07-08 10:24:12 +0800

    Fix key issue while installing melodic

--- scripts/install_melodic.sh
+++ scripts/install_melodic.sh
@@ -4,7 +4,7 @@ ros1_distro=melodic
 
     # Install ROS 1
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-    sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
     sudo apt update
     sudo apt install -y ros-$ros1_distro-desktop-full
 
