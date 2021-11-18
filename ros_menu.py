b70823db687a05dce897d1b2c9d86c76ac2bf049
ChenYing Kuo <evshary@gmail.com>
2021-11-18 15:57:46 +0800

    Add PS1 for prompt.

--- ros_menu.py
+++ ros_menu.py
@@ -106,6 +106,7 @@ def check_bridge():
 def create_ros_sourcefile(source_file, filename):
     ros_source_file = open(filename, 'w')
     ros_source_file.write("shell=`cat /proc/$$/cmdline | tr -d '\\0' | tr -d '-'`\n")
+    ros_source_file.write("PS1=\"(%s) $PS1\"\n" % choose)
     if (source_file['Menu'][choose]['ROS_version'] == 1):
         ros_source_file.write(source_ros1()+read_cmds())
     if (source_file['Menu'][choose]['ROS_version'] == 2):
